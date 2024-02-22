dc082478a1f01a7d1339cbb120ea330bc34697bf
Loogeek <xiaolong3956@gmail.com>
2024-02-22 12:53:49 +0800

    feat: add lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './challenges/01-helloWorld/index.vue'
+// import HelloWorld from './challenges/01-helloWorld/index.vue'
+import LifecycleHooks from './challenges/02-lifecycleHooks/index.vue'
 </script>
 
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
+  <LifecycleHooks />
 </template>
 
 <style scoped></style>

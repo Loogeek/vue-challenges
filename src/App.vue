7f4b863734268175d7ee978e289ed0267f82dbd0
Loogeek <xiaolong3956@gmail.com>
2024-02-23 00:19:28 +0800

    feat: add next dom update flush

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 // import HelloWorld from './challenges/01-helloWorld/index.vue'
-import LifecycleHooks from './challenges/02-lifecycleHooks/index.vue'
+// import LifecycleHooks from './challenges/02-lifecycleHooks/index.vue'
+import NextDomUpdateFlush from './challenges/03-nextDOMUpdateFlush/index.vue'
 </script>
 
 <template>
   <!-- <HelloWorld /> -->
-  <LifecycleHooks />
+  <!-- <LifecycleHooks /> -->
+  <NextDomUpdateFlush />
 </template>
 
 <style scoped></style>
---
+++ src/challenges/03-nextDOMUpdateFlush/index.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, nextTick } from "vue"
+
+const count = ref(0)
+const counter = ref(null)
+
+async function increment() {
+  count.value++
+
+  /**
+   * DOM is not yet updated, how can we make sure that the DOM gets updated
+   * Make the output be true
+  */
+  // 解法一：
+  await nextTick()
+  console.log(+counter.value.textContent === 1)
+
+  // 解法二：
+  nextTick(() => {
+    console.log(+counter.value.textContent === 1)
+  })
+}
+</script>
+
+<template>
+  <button ref="counter" @click="increment">
+    {{ count }}
+  </button>
+</template>

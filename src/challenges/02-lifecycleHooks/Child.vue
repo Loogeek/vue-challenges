fed8984369f47dcf87f43e06a442d570fddcce4f
Loogeek <xiaolong3956@gmail.com>
2024-02-22 12:54:04 +0800

    feat: add lifecycle hooks

---
+++ src/challenges/02-lifecycleHooks/Child.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    Child Component: {{ count }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount, inject, Ref } from 'vue'
+
+const timer = inject<Ref>("timer")
+const count = inject<Ref>("count")
+
+onMounted(() => {
+  timer!.value = window.setInterval(() => {
+    count!.value++
+  }, 1000)
+})
+
+onBeforeUnmount(() => {
+  if (timer) {
+    timer.value && clearInterval(timer.value)
+  }
+})
+
+</script>
+
+<style scoped></style>
---
+++ src/challenges/02-lifecycleHooks/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Child v-if="visible" />
+
+    <div>
+      <button @click="handleToggle">
+        Toggle Child Component
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { provide, ref } from 'vue'
+import Child from './Child.vue'
+
+const visible = ref(true)
+const count = ref(0)
+const timer = ref(null)
+
+provide('timer', timer)
+provide('count', count)
+
+function handleToggle() {
+  visible.value = !visible.value
+}
+</script>
+
+<style scoped></style>

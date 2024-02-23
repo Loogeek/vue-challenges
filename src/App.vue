4f970f32161817714acd9d4f3fe2ef208f533767
Loogeek <xiaolong3956@gmail.com>
2024-02-23 10:52:43 +0800

    feat: add dom portal

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 // import HelloWorld from './challenges/01-helloWorld/index.vue'
 // import LifecycleHooks from './challenges/02-lifecycleHooks/index.vue'
-import NextDomUpdateFlush from './challenges/03-nextDOMUpdateFlush/index.vue'
+// import NextDomUpdateFlush from './challenges/03-nextDOMUpdateFlush/index.vue'
+import DOMPortal from './challenges/04-domPortal/index.vue'
 </script>
 
 <template>
   <!-- <HelloWorld /> -->
   <!-- <LifecycleHooks /> -->
-  <NextDomUpdateFlush />
+  <!-- <NextDomUpdateFlush /> -->
+  <DOMPortal />
 </template>
 
 <style scoped></style>
